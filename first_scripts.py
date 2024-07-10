ead5434084c14dd1866d10d0432985d2785fd76f
dbamfo <bamfodaniel@yahoo.com>
2024-07-10 15:29:21 +0100

    added feature

--- first_scripts.py
+++ first_scripts.py
@@ -3,3 +3,5 @@ print(vac)
 
 print("linda")
 print('bamfo')
+print(vac,", " "Mr Bamfo")
+print( "dev","stage")
--- variables.py
+++ variables.py
@@ -16,15 +16,17 @@ print(var)
 #4. variable bool
 var = "true"
 print(var)
+print(type(var))
 
 #printing multiple assignment with multi value
-x,y, z = "bamf","lin","yve"
+x,y, z = "bamf","lin",8
 print("variable x is", x)
 print("variable y is",y)
 print("variable z is",z)
-
+print("multiple variable assignment x,y,z are",x,y,z)
+print(type(x),type(z),type(y))
 #The print method called type: indicate the type of variable assigned
-z = 12
-x = "alpha"
-print(type(z))
-print(type(x))
+a = 12
+b = "alpha"
+print(type(a))
+print(type(b))
